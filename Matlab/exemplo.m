b303cab383c8a244f04380ed625284aff4bb10ae
douglaskataki <douglaskataki@hotmail.com>
2020-08-01 08:16:05 -0300

    ajeitando alguns parametros
    Fiz alterações para que uma figura contenha duas figuras, uma sem e uma com reflexão de primeira ordem.
    Coloquei também o plot do SNR considerando modulação OOK

--- Matlab/exemplo.m
+++ Matlab/exemplo.m
@@ -58,44 +58,46 @@ P_Rx = PLed*G_Con.*H;
 % para dBm
 P_Rx_dBm = 10*log10(P_Rx);
 
-% %% SNR
-% % ruido
-% % Dados retirados Fundamental Analysis for Visible-Light Communication System using LED Lights
-% q = 1.6E-19; % Carga do eletron
-% k = physconst('Boltzmann');
-% c = physconst('LightSpeed');
-% % banda (100Mb/s)
-% B = 100E6; %Mbps
-% % Responsividade do Fotodetector
-% R = 0.54; % A/W
-% % photocorrent due to ground radiation
-% I_b = 5400E-6; % uA
-% % temperatura ambiente
-% T = 25; % C
-% T_k = T+273; % K
-% % % open-loop voltage gain
-% Gol = 10;
-% % fixed capacitance of photodetector per unit area 
-% Cpd = 112*10^-12/10^-4; %pF/cm^2
-% % channel noise factor 
-% Gamma = 1.5;
-% % FET transconducatance  
-% gm = 30E-3; % mS
-% % noise bandwidth factor
-% I_2 = .562;
-% I_3 = .0868;
-% % ruidos shot e thermal
-% sigma2_shot = 2*q*R.*P_Rx*B+2*q*I_b*I_2*B; 
-% sigma2_thermal = (8*pi*k*T_k/Gol)*Cpd*Area*I_2*B^2 + (16*pi^2*k*T_k*Gamma/gm)*Cpd^2*Area^2*I_3*B^3;
-% % background
-% pbn = 5.8e-6/1e-4 %uW/cm^2
-% d_l = 30e-9 %nm
-% sigma2_background = 2*q*I_2*pbn*Area*d_l*R*B;
-% noise2 = sigma2_shot+sigma2_thermal+sigma2_background;
-% SNR = R^2*P_Rx.^2./noise2;
-% SNR_dB = 10*log10(SNR);
+%% SNR
+% ruido
+% Dados retirados Fundamental Analysis for Visible-Light Communication System using LED Lights
+q = 1.6E-19; % Carga do eletron
+k = physconst('Boltzmann');
+c = physconst('LightSpeed');
+% banda (100Mb/s)
+B = 100E6; %Mbps
+% Responsividade do Fotodetector
+R = 0.54; % A/W
+% photocorrent due to ground radiation
+I_b = 5400E-6; % uA
+% temperatura ambiente
+T = 25; % C
+T_k = T+273; % K
+% % open-loop voltage gain
+Gol = 10;
+% fixed capacitance of photodetector per unit area 
+Cpd = 112*10^-12/10^-4; %pF/cm^2
+% channel noise factor 
+Gamma = 1.5;
+% FET transconducatance  
+gm = 30E-3; % mS
+% noise bandwidth factor
+I_2 = .562;
+I_3 = .0868;
+% ruidos shot e thermal
+sigma2_shot = 2*q*R.*P_Rx*B+2*q*I_b*I_2*B; 
+sigma2_thermal = (8*pi*k*T_k/Gol)*Cpd*Area*I_2*B^2 + (16*pi^2*k*T_k*Gamma/gm)*Cpd^2*Area^2*I_3*B^3;
+% background (fundo)
+pbn = 5.8e-6/1e-4;  %uW/cm^2
+d_l = 30e-9; %nm
+sigma2_background = 2*q*I_2*pbn*Area*d_l*R*B;
+noise2 = sigma2_shot+sigma2_thermal+sigma2_background;
+% considerando que estamos utilizando OOK com Ps0 = 0 e P_Rx = Ps1
+SNR = R^2*P_Rx.^2./noise2;
+SNR_dB = 10*log10(SNR);
+
 %% Plot do diagrama de potencia em dBm
-figure(1)
+subplot(1,2,1)
 meshc(x,y,P_Rx_dBm);
 grid on;
 title('LOS - LED');
@@ -103,8 +105,8 @@ xlabel('x(m)');
 ylabel('y(m)');
 zlabel('Potencia(dBm)')
 colorbar;
-colormap jet;
-figure(2)
+
+subplot(1,2,2)
 % com reflexao
 rho = 0.8;
 H = H + HNLOS(xl,yl,zl,rho,Area,FOV,n,Nx,Ny,Nz,x,y,z,N_Tx,Pos_Tx,N_Rx);
@@ -112,5 +114,13 @@ P_Rx = PLed*G_Con.*H;
 P_Rx_dBm = 10*log10(P_Rx);
 meshc(x,y,P_Rx_dBm);
 colorbar;
-colormap winter;
+colormap jet;
 
+figure(2)
+meshc(x,y,SNR_dB);
+title('LOS - LED');
+xlabel('x(m)');
+ylabel('y(m)');
+zlabel('SNR(dBm)')
+colorbar;
+colormap jet;
